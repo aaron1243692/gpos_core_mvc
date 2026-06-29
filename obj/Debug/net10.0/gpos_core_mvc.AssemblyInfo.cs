96c1ef65172c8965b41ab48017afd34e2ed2a85f
gtechjimsoncalibuso-source <gtechjimsoncalibuso@gmail.com>
2026-06-29 12:26:49 +0700

    back

--- obj/Debug/net10.0/gpos_core_mvc.AssemblyInfo.cs
+++ obj/Debug/net10.0/gpos_core_mvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gpos_core_mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2b751fa8f2d1a5ef72b0955f339e42a52145b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425f10f998796255074d9bfce71ddf46022cf5e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("gpos_core_mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gpos_core_mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
